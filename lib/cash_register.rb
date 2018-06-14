5da077ef76919524a3ff4208308820a78e28a75e
Reno Sorensen <renojames19@gmail.com>
2018-06-14 02:03:04 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,4 +9,17 @@ class CashRegister
     @discount = discount
   end
 
+  def add_item(title, price, qty = 0)
+    if qty > 0
+      @total = @total + (price*qty)
+    else
+      @total = @total + price
+    end
+  end
+
+  def apply_discount
+    @total = @total - (@total.to_f*(@discount.to_f/100))
+    "After the discount, the total comes to $#{@total.to_i}."
+  end
+
 end
